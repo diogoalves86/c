4b578b3a8ee5b371d74e923f82632c7f3dde463b
Diogo Alves <diogoalvessilva86@gmail.com>
2018-05-19 01:48:51 -0300

    Refatorando código

--- estudosGerais/registroUtils.h
+++ estudosGerais/registroUtils.h
@@ -12,3 +12,22 @@ long getTamanhoArquivo (FILE *arquivo){
 	fseek(arquivo, 0, SEEK_END);
 	return ftell(arquivo);
 }
+
+void exibeDados(Registro *registro){
+	printf("%.8s\n", registro->chave);
+	printf("%.40s\n", registro->nome);
+	printf("%.40s\n", registro->email);
+	printf("%.12s\n", registro->telefone);
+}
+
+void formataDados(Registro *registro, int contador){
+	sprintf(registro->chave, "chav%.2d", contador);
+	sprintf(registro->nome, "esteeumnomelegal%.2d", contador);
+	sprintf(registro->email, "esteeumemailegal%.2d", contador);
+	sprintf(registro->telefone, "tel%.2d", contador);
+}
+
+void insereNoArquivo(FILE *arquivo, Registro *registro){
+	
+	fprintf(arquivo, "%.8s%.40s%.40s%.12s\n", registro->chave, registro->nome, registro->email, registro->telefone);
+}
--- estudosGerais/script-cria-arquivo-questao-1.c
+++ estudosGerais/script-cria-arquivo-questao-1.c
@@ -8,15 +8,9 @@ int main (void){
 
 	for (int i = 0; i < 100; i++)
 	{
-		sprintf(registro.chave, "chav%.2d", i);
-		sprintf(registro.nome, "esteeumnomelegal%.2d", i);
-		sprintf(registro.email, "esteeumemailegal%.2d", i);
-		sprintf(registro.telefone, "tel%.2d", i);
-		printf("%.8s\n", registro.chave);
-		printf("%.40s\n", registro.nome);
-		printf("%.40s\n", registro.email);
-		printf("%.12s\n", registro.telefone);
-		fprintf(arquivo, "%.8s%.40s%.40s%.12s\n", registro.chave, registro.nome, registro.email, registro.telefone);
+		formataDados(&registro, i);
+		exibeDados(&registro);
+		insereNoArquivo(arquivo, &registro);
 		printf("Registro inserido com sucesso! :D\n");
 	}
 	printf("Arquivo criado com sucesso!\n");
